2959d119730b30435e498c8db36664dcd8a189da
shancsevz <shanmugamcse@yahoo.co.in>
2026-03-12 01:20:39 +0100

    [Shan] - Code and UT Fixes

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/WarehouseRepository.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/WarehouseRepository.java
@@ -30,20 +30,31 @@ public class WarehouseRepository implements WarehouseStore, PanacheRepository<Db
 
   @Override
   public void update(Warehouse warehouse) {
-    // Task-3
-    DbWarehouse dbWarehouse = find("businessUnitCode", warehouse.businessUnitCode).firstResult();
-    if(dbWarehouse  != null){
-      String Query = "UPDATE DbWarehouse dw SET dw.location = :loc, dw.capacity = :cap, " +
-              "dw.stock = :stk, dw.archivedAt = :archat WHERE dw.businessUnitCode = :bucode";
+    getEntityManager().createQuery(
+                    "UPDATE DbWarehouse w SET w.location = :loc, w.capacity = :cap, " +
+                            "w.stock = :stock, w.archivedAt = :archived WHERE w.businessUnitCode = :code")
+            .setParameter("loc", warehouse.location)
+            .setParameter("cap", warehouse.capacity)
+            .setParameter("stock", warehouse.stock)
+            .setParameter("archived", warehouse.archivedAt)
+            .setParameter("code", warehouse.businessUnitCode)
+            .executeUpdate();
 
-      Parameters parameters = new Parameters();
-      parameters.and("loc",warehouse.location);
-      parameters.and("cap",warehouse.capacity);
-      parameters.and("stk",warehouse.stock);
-      parameters.and("archat",warehouse.archivedAt);
-      parameters.and("bucode",warehouse.businessUnitCode);
-      this.update(Query,parameters);
-    }
+    // Clear persistence context to see updates in subsequent queries
+    getEntityManager().flush();
+    getEntityManager().clear();
+  }
+
+  @Override
+  public void updateStock(Warehouse warehouse) {
+    getEntityManager().createQuery(
+                    "UPDATE DbWarehouse w SET w.stock = :stock WHERE w.businessUnitCode = :code")
+            .setParameter("stock", warehouse.stock)
+            .executeUpdate();
+
+    // Clear persistence context to see updates in subsequent queries
+    getEntityManager().flush();
+    getEntityManager().clear();
   }
 
   @Override
